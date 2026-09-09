44f4cfd2657c36ccf9a4fab471575f6418ec7a8a
LisaYllander92 <Lisaylander92@gmail.com>
2026-09-09 12:03:40 +0200

    more exercises

--- exercises/0_0_google_queries.sql
+++ exercises/0_0_google_queries.sql
@@ -1,3 +1,7 @@
+/*Go into marketplace under data products in snowsight. 
+Search and get the following dataset Google Keywords search dataset - discover all searches on Google.
+Now create a worksheet on your local repository and start querying this data through vscode.
+a) Use this database and find out the underlying schemas, tables and views to get an overview of its logical structure.?*/
 -- Vilka warehouses finns?
 SHOW WAREHOUSES;
 
@@ -16,15 +20,42 @@ SHOW SCHEMAS;
 --Välj schema
 USE SCHEMA DATAFEEDS;
 
--- Vilka tables finns
+-- Vilka tables finns - NAME GOOGLE_KEYWORDS (table)
 SHOW TABLES IN SCHEMA DATAFEEDS;
 
--- KOlla kolumner och datatyper
+-- b) Find out the columns and its data types in the table GOOGLE_KEYWORDS.
+--Kolla kolumner och datatyper i google_keywords
 DESCRIBE TABLE google_keywords;
-
 
 SELECT
     *
 FROM
     GOOGLE_KEYWORDS
-LIMIT 10;
+LIMIT
+    10;
+
+/*We will now do some exploratory data analysis (EDA) of this dataset.
+c) Find out number of rows in the dataset.*/
+SELECT
+    COUNT(*) AS num_rows
+FROM
+    google_keywords;
+
+-- d) When is the first search and when is the latest search in the dataset?
+SELECT
+    MIN(DATE) AS first_date,
+    MAX(DATE) AS last_date
+FROM
+    google_keywords;
+
+-- e) Which are the 10 most popular keywords?
+SELECT
+    KEYWORD
+FROM
+    google_keywords
+GROUP BY
+    KEYWORD
+ORDER BY
+    COUNT(*)
+LIMIT
+    10;
--- snowflake_setup/sql_scripts/testing.sql
+++ snowflake_setup/sql_scripts/testing.sql
@@ -4,6 +4,8 @@ USE snowflake_sample_data;
 
 SHOW SCHEMAS;
 
+
+
 SHOW TABLES IN SCHEMA TPCH_SF1;
 
 SELECT * FROM snowflake_sample_data.tpch_sf1.customer;
